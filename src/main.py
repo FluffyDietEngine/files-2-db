060020d327719a9f9e938f960a17a12f6a3056e3
Santhosh Solomon <santhosh@advarisk.com>
2024-02-04 19:53:52 +0530

    file to df conversion

--- src/main.py
+++ src/main.py
@@ -1,15 +1,55 @@
 from pathlib import Path
 from argparse import ArgumentParser
 
+from polars import scan_csv, LazyFrame
+
+
+def convert_csv_to_df(
+    file_path: str,
+    seperator: str,
+    file_has_headers: bool,
+    ignore_file_errors: bool,
+    n_rows_to_consider: int,
+) -> LazyFrame:
+    """Converts the input file to dataframe using polars LazyFrame to handle bigger than RAM size files
+
+    Args:
+        file_path (str): Absolute path to the file
+        seperator (str): Column seperator
+        file_has_headers (bool): File have headers to consider
+        ignore_file_errors (bool): Try to keep reading lines if some lines yield errors
+        n_rows_to_consider (int): Number of rows to consider
+
+    Returns:
+        LazyFrame: LazyFrame object of polars
+    """
+    file_to_df = scan_csv(
+        source=file_path,
+        separator=seperator,
+        has_header=file_has_headers,
+        ignore_errors=ignore_file_errors,
+        n_rows=n_rows_to_consider,
+    )
+    return file_to_df
+
 
 def validate_file(file_path: Path) -> None:
+    """Validation method for the input file
+    with file type and file extension
+
+    Args:
+        file_path (str): Absolute path to the file
+
+    Raises:
+        ValueError: Not a valid file location
+        TypeError: Not acceptable file type
+
+    """
     if not file_path.is_file():
-        raise TypeError("Input file doesn't exists.")
+        raise ValueError("Input file doesn't exists.")
 
     if file_path.suffix not in [".csv"]:
         raise TypeError(f"Input file type => '{file_path.suffix}' is not supprted")
-
-    return None
 
 
 def blue_print(
@@ -22,12 +62,34 @@ def blue_print(
     file_seperator: str,
     file_has_header: bool,
     ignore_file_errors: bool,
-    n_rows_to_insert: int,
-):
+    n_rows_to_consider: int,
+) -> None:
+    """_summary_
+
+    Args:
+        file_path (str): Absolute path to the file
+        target_database (str): Target database type
+        db_host (str): URL or IP address representation of database host
+        db_port (int): Port number to connect to the database
+        db_user (str): Database username for authentication
+        db_password (str): Database password for authentication
+        file_seperator (str): Column seperator
+        file_has_header (bool): File have headers to consider
+        ignore_file_errors (bool): Try to keep reading lines if some lines yield errors
+        n_rows_to_consider (int): Number of rows to consider
+    """
+
     # validate file path and extension
     validate_file(Path(file_path))
 
     # read as dataframe
+    _dataframe = convert_csv_to_df(
+        file_path=file_path,
+        seperator=file_seperator,
+        file_has_headers=file_has_header,
+        ignore_file_errors=ignore_file_errors,
+        n_rows_to_consider=n_rows_to_consider,
+    )
 
     # pass to query builder to insert
 
@@ -77,11 +139,13 @@ def main():
         "--has_header",
         default=True,
         help="Indicate the file contains header. default value -> True",
+        type=bool,
     )
     arg_parser.add_argument(
         "--ignore_errors",
         default=False,
         help="Try to keep reading lines if some lines yield errors. default value -> False",
+        type=bool,
     )
     arg_parser.add_argument(
         "--n_rows",
@@ -89,7 +153,6 @@ def main():
         help="Number of rows from the file to be inserted to database. Default -> all",
     )
 
-    # import pdb; pdb.set_trace()
     parameters = arg_parser.parse_args()
 
     blue_print(
@@ -107,6 +170,4 @@ def main():
 
 
 if __name__ == "__main__":
-    file_path = Path("/Users/santhoshsolomon/Desktop/Final Hollywood.csv")
     main()
-    # main(source_file_path=file_path, file_type=".csv")
